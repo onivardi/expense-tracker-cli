614dd0fb7fdd30fb58dc584453e1dbb2f488ee4a
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2026-02-19 19:35:01 -0400

    Add list command
    List command display the entire data using pandas

--- src/expense_tracker/cli.py
+++ src/expense_tracker/cli.py
@@ -1,8 +1,29 @@
 import sys
+import argparse
+import pandas as pd
+from pathlib import Path
 
 
 def main() -> None:
-    print("Hello from expense-tracker!")
+    parser = argparse.ArgumentParser(description="Manage expenses.")
+    subparsers = parser.add_subparsers(
+        help="Available commands", dest="command", required=True
+    )
+
+    # List subcommand
+    subparsers.add_parser("list", help="List a table of expensses")
+
+    args = parser.parse_args()
+
+    if args.command == "list":
+        list()
+
+
+def list():
+    file_path = Path(__file__).parent / "my-expenses.csv"
+
+    data = pd.read_csv(file_path)
+    print(data.to_string(index=False))
 
 
 if __name__ == "__main__":
