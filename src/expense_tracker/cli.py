e833bf6877020554ea4f6c7782a2013e37468ec5
Valdeir Rodrigues <vardii.rodrigues@gmail.com>
2026-02-20 11:22:16 -0400

    Refact list and add_expense functions
    Created two functions that read and save the csv file

--- src/expense_tracker/cli.py
+++ src/expense_tracker/cli.py
@@ -26,23 +26,20 @@ def main() -> None:
     args = parser.parse_args()
 
     if args.command == "list":
-        display_expenses()
+        display = display_expenses()
+        print(display)
     elif args.command == "add":
         res = add_expense(args.description, args.amount)
         print(res)
 
 
-def display_expenses():
-    file_path = Path(__file__).parent / "my-expenses.csv"
-
-    data = pd.read_csv(file_path)
-    print(data.to_string(index=False))
+def display_expenses() -> str:
+    data = load_data()
+    return data.to_string(index=False)
 
 
 def add_expense(description: str, amount: str) -> str:
-    file_path = Path(__file__).parent / "my-expenses.csv"
-
-    data = pd.read_csv(file_path)
+    data = load_data()
     id = data["ID"].iloc[-1] + 1
 
     new_entry = pd.DataFrame(
@@ -55,9 +52,19 @@ def add_expense(description: str, amount: str) -> str:
             }
         ]
     )
-    new_entry.to_csv(file_path, mode="a", header=False, index=False)
-
+    save_data(new_entry)
     return f"Expense added successfully (ID: {id})"
+
+
+def load_data() -> pd.DataFrame:
+    file_path = Path(__file__).parent / "my-expenses.csv"
+
+    return pd.read_csv(file_path)
+
+
+def save_data(new_data: pd.DataFrame) -> None:
+    file_path = Path(__file__).parent / "my-expenses.csv"
+    new_data.to_csv(file_path, mode="a", header=False, index=False)
 
 
 if __name__ == "__main__":
